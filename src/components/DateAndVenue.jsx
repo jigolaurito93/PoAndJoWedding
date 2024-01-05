9aced6b1681099c8a39a37995749153c76933161
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 04:50:02 -0600

    2nd commit

--- src/components/DateAndVenue.jsx
+++ src/components/DateAndVenue.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import weddingRing from "../assets/wedding ring.png";
 
 function DateAndVenue() {
   return (
-    <div>
-      
+    <div id="weddingDetails" className="flex  justify-center">
+      <div>
+        <img src={weddingRing} alt="" />
+      </div>
+      <div>
+        <h3>Eleventh of June, Tuesday</h3>
+        <h4>at the</h4>
+        <h2>Farm Hills Garden</h2>
+        <div>
+          <h3>Barangay</h3>
+          <h3>Silang, Cavite</h3>
+          <h3>Philippines</h3>
+        </div>
+        <h2>Three O'Clock</h2>
+        <h4>in the afternoon</h4>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DateAndVenue
+export default DateAndVenue;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import LandingHero from '../components/LandingHero'
+import React from "react";
+import LandingHero from "../components/LandingHero";
+import DateAndVenue from "../components/DateAndVenue";
 
 function Home() {
   return (
     <div>
       <LandingHero />
+      <DateAndVenue />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
