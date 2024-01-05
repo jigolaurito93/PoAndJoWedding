3b72516c9898b382e9d953ac0b84b377ce56f1c0
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 15:16:38 -0600

    changed display to relative

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -3,8 +3,8 @@ import flowers from "../assets/flowers.png";
 
 function LandingHero() {
   return (
-    <div className="w-full min-h-screen" id="heroContainer">
-      <div className="flex flex-col justify-center items-center px-3 pt-52 gap-4">
+    <div className="w-full min-h-screen px-3 pt-52" id="heroContainer relative">
+      <div className="flex flex-col justify-center items-center  gap-4">
         <h1 className="text-6xl md:text-8xl lg:text-9xl">Polo and Joie</h1>
         <h2 className="text-5xl mt-3 mb-16 md:text-6xl lg:text-7xl">
           together forever
