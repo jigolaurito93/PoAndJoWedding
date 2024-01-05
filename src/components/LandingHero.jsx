753f9e12061a2c7c650af8a55bb61b1970344627
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 14:59:46 -0600

    adjust date

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -6,7 +6,7 @@ function LandingHero() {
     <div className="w-full h-screen" id="heroContainer">
       <div className="flex flex-col justify-center items-center px-3 pt-[50%] gap-4">
         <h1 className="text-6xl md:text-8xl lg:text-9xl">Polo and Joie</h1>
-        <h2 className="text-5xl mt-3 mb-36 md:text-6xl lg:text-7xl">
+        <h2 className="text-5xl mt-3 mb-16 md:text-6xl lg:text-7xl">
           together forever
         </h2>
         <h2 className="text-4xl">
