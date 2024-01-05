54e05e6110fc7634164a1599e5c6b604c6995ec3
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 04:56:15 -0600

    details page structure done

--- src/components/DateAndVenue.jsx
+++ src/components/DateAndVenue.jsx
@@ -3,20 +3,23 @@ import weddingRing from "../assets/wedding ring.png";
 
 function DateAndVenue() {
   return (
-    <div id="weddingDetails" className="flex  justify-center">
+    <div
+      id="weddingDetails"
+      className="flex gap-14 justify-center items-center py-20"
+    >
       <div>
         <img src={weddingRing} alt="" />
       </div>
-      <div>
+      <div className="flex flex-col justify-center items-center">
         <h3>Eleventh of June, Tuesday</h3>
         <h4>at the</h4>
-        <h2>Farm Hills Garden</h2>
+        <h3>Farm Hills Garden</h3>
         <div>
-          <h3>Barangay</h3>
-          <h3>Silang, Cavite</h3>
-          <h3>Philippines</h3>
+          <h4>Barangay</h4>
+          <h4>Silang, Cavite</h4>
+          <h4>Philippines</h4>
         </div>
-        <h2>Three O'Clock</h2>
+        <h3>Three O'Clock</h3>
         <h4>in the afternoon</h4>
       </div>
     </div>
