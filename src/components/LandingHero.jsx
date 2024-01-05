bb2847ced46e203d2a1ce4e03a8d1948eed5419d
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 14:47:50 -0600

    adjusted hero text

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import flowers from '../assets/flowers.png'
+import flowers from "../assets/flowers.png";
 
 function LandingHero() {
   return (
-    <div className="w-full pt-20 h-[100%]" id="heroContainer">
-      <div className="flex flex-col justify-center items-center mt-20 px-3">
+    <div className="w-full h-screen border-2 border-black" id="heroContainer">
+      <div className="flex flex-col justify-center items-center px-3 pt-[50%]">
         <h1 className="text-6xl md:text-8xl lg:text-9xl">Polo and Joie</h1>
-        <h2 className="text-5xl mt-3 mb-36 md:text-6xl lg:text-7xl">together forever</h2>
+        <h2 className="text-5xl mt-3 mb-36 md:text-6xl lg:text-7xl">
+          together forever
+        </h2>
         <img src={flowers} alt="" />
         <h1></h1>
       </div>
