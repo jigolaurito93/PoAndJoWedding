a32e985f68cf8e50d39f5b2d363d6a9b0810c512
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 07:40:16 -0600

    mobile responsive for section 2

--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -5,22 +5,24 @@ function DateAndVenue() {
   return (
     <div
       id="weddingDetails"
-      className="flex gap-14 justify-center items-center py-20"
+      className="flex flex-col gap-9 lg:gap-16 lg:flex-row lg:py-28 xl:gap-28 justify-center items-center py-20"
     >
       <div>
-        <img src={weddingRing} alt="" />
+        <img src={weddingRing} alt="" className="w-52 xl:w-72" />
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <h3>Eleventh of June, Tuesday</h3>
-        <h4>at the</h4>
-        <h3>Farm Hills Garden</h3>
-        <div>
+      <div className="flex gap-4 flex-col justify-center items-center md:gap-7">
+        <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl">
+          Eleventh of June, Tuesday
+        </h3>
+        <h4 className="tracking-wider md:text-2xl">at the</h4>
+        <h3 className="text-3xl  md:text-5xl lg:text-6xl xl:text-7xl">Farm Hills Garden</h3>
+        <div className="tracking-wider md:text-2xl">
           <h4>Barangay</h4>
           <h4>Silang, Cavite</h4>
           <h4>Philippines</h4>
         </div>
-        <h3>Three O'Clock</h3>
-        <h4>in the afternoon</h4>
+        <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl">Three O'Clock</h3>
+        <h4 className="tracking-wider md:text-2xl">in the afternoon</h4>
       </div>
     </div>
   );
