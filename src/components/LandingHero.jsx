ab9aaa39918b63d7226e24beb1e2470c2442bd43
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 15:22:24 -0600

    fixed bug alignment

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -3,16 +3,16 @@ import flowers from "../assets/flowers.png";
 
 function LandingHero() {
   return (
-    <div className="w-full min-h-screen px-3 pt-52" id="heroContainer relative">
+    <div className="w-full min-h-screen px-3 pt-52" id="heroContainer">
       <div className="flex flex-col justify-center items-center  gap-4">
         <h1 className="text-6xl md:text-8xl lg:text-9xl">Polo and Joie</h1>
         <h2 className="text-5xl mt-3 mb-16 md:text-6xl lg:text-7xl">
           together forever
         </h2>
-        <h2 className="text-4xl">
-          6 <span>.</span> 11 <span>.</span> 24
-        </h2>
-        <img src={flowers} alt="" className="absolute bottom-0" />
+        <h2 className="text-4xl">6 . 11 . 24</h2>
+        <div className="absolute bottom-0">
+          <img src={flowers} alt="" />
+        </div>
       </div>
     </div>
   );
