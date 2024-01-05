51b6793851661b3b8c4fa4141ccedf20686a02f2
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-05 14:56:36 -0600

    second hero text adjustment

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -3,14 +3,16 @@ import flowers from "../assets/flowers.png";
 
 function LandingHero() {
   return (
-    <div className="w-full h-screen border-2 border-black" id="heroContainer">
-      <div className="flex flex-col justify-center items-center px-3 pt-[50%]">
+    <div className="w-full h-screen" id="heroContainer">
+      <div className="flex flex-col justify-center items-center px-3 pt-[50%] gap-4">
         <h1 className="text-6xl md:text-8xl lg:text-9xl">Polo and Joie</h1>
         <h2 className="text-5xl mt-3 mb-36 md:text-6xl lg:text-7xl">
           together forever
         </h2>
-        <img src={flowers} alt="" />
-        <h1></h1>
+        <h2 className="text-4xl">
+          6 <span>.</span> 11 <span>.</span> 24
+        </h2>
+        <img src={flowers} alt="" className="absolute bottom-0" />
       </div>
     </div>
   );
